1f963c24b8d7ff0db200069f17e450ed7a68ab47
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-08-18 16:33:20 -0700

    Ignore minutes items

--- python/cdp_mountain_view_backend/scraper.py
+++ python/cdp_mountain_view_backend/scraper.py
@@ -18,6 +18,18 @@ def get_events(
     scraper = LegistarScraper(
         client="mountainview",
         timezone="America/Los_Angeles",
+        ignore_minutes_item_patterns=[
+            (
+                "During this declared state of emergency, "
+                "the meeting will be conducted in accordance"
+            ),
+            "the City Council participated in the meeting by video conference",
+            (
+                "There is a 90-day limit for the filing of a challenge "
+                "in Superior Court to certain City administrative decisions"
+            ),
+            "CONSENT CALENDAR - None.",
+        ],
     )
 
     return scraper.get_events(begin=from_dt, end=to_dt)
